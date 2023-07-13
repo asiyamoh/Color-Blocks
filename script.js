29a17113f46cc4c2f6e15609af3fa980f604b232
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-13 17:10:43 -0500

    add the red button in scrpit.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('🟥 🟦 🟩 🟨');
+
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('🟥 🟦 🟩 🟨');
+    $("#red-btn").on('click', addRedColorBlock);
+}
+
+function addRedColorBlock(){
+    $('#blocks').append(`<div class="block red-fill"></div>`);
+}
