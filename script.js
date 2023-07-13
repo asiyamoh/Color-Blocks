006197bc08312d053728f6cac8eb48ed6decbe8d
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-13 17:22:30 -0500

    add blue,green,yellow block function

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('🟥 🟦 🟩 🟨');
     $("#red-btn").on('click', addRedColorBlock);
+    $("#blue-btn").on('click', addBlueColorBlock);
+    $("#green-btn").on('click', addGreenBlock);
+    $('#yellow-btn').on('click', addYellowBlock);
+
 }
 
 function addRedColorBlock(){
     $('#blocks').append(`<div class="block red-fill"></div>`);
+}
+
+function addBlueColorBlock(){
+    $('#blocks').append(`<div class="block blue-fill"></div>`);
+}
+
+function addGreenBlock(){
+    $('#blocks').append(`<div class="block green-fill"></div>`);
+}
+
+function addYellowBlock(){
+    $('#blocks').append(`<div class="block yellow-fill"></div>`);
 }
