e285f20041c05c33e980d2d625c30518f144de9c
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-13 22:44:38 -0500

    added a color counter unodate in js

--- script.js
+++ script.js
@@ -8,21 +8,58 @@ function readyNow(){
     $("#blue-btn").on('click', addBlueColorBlock);
     $("#green-btn").on('click', addGreenBlock);
     $('#yellow-btn').on('click', addYellowBlock);
+   // $('#blocks').on('click','.red-fill',removeRedColorBlock);
+    // $('#blocks').on('click','.blue-fill',removeBlueColorBlock);
+    // $('#blocks').on('click','.green-fill',removeGreenColorBlock);
+    // $('#blocks').on('click','.yellow-fill',removeYellowColorBlock);
 
+
+    
 }
-
+let redCounter = 1;
 function addRedColorBlock(){
     $('#blocks').append(`<div class="block red-fill"></div>`);
+    redCounter++;
+    $('#red-count').text(redCounter);
 }
 
+let blueCounter =1;
 function addBlueColorBlock(){
     $('#blocks').append(`<div class="block blue-fill"></div>`);
+    blueCounter++;
+    $('#blue-count').text(blueCounter);
 }
 
+let greenCounter =1;
 function addGreenBlock(){
     $('#blocks').append(`<div class="block green-fill"></div>`);
+    greenCounter++;
+    $('#green-count').text(greenCounter);
 }
 
+let yellowCounter =1;
 function addYellowBlock(){
     $('#blocks').append(`<div class="block yellow-fill"></div>`);
+    yellowCounter++;
+    $('#yellow-count').text(yellowCounter);
 }
+
+
+
+
+
+// function removeRedColorBlock(){
+//     $(this).remove();
+// }
+
+// function removeBlueColorBlock(){
+//     $(this).remove();
+// }
+
+// function removeGreenColorBlock(){
+//     $(this).remove();
+// }
+
+// function removeYellowColorBlock(){
+//     $(this).remove();
+// }
